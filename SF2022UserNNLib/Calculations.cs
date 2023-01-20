d6eda0ba1697645a8e0465f01d3cd3bb15a4e3d2
SashaStark <84183933+SashaStarkova@users.noreply.github.com>
2023-01-20 15:08:43 +0500

    Add files via upload

---
+++ SF2022UserNNLib/Calculations.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF2022UserNNLib
+{
+    public class Calculations
+    {
+        public string[] AvailablePeriods(TimeSpan[] StartTimes, int[] Durations, TimeSpan BeginWorkingTime, TimeSpan EndWorkingTime, int ConsultationTime)
+        {
+            int i = 0;
+            TimeSpan workdur = EndWorkingTime - BeginWorkingTime;
+            int a = Convert.ToInt32(workdur.Ticks / new TimeSpan(0, ConsultationTime, 0).Ticks);
+            TimeSpan[] endtime = new TimeSpan[StartTimes.Length];
+            string[] retst= new string[a];
+            foreach (TimeSpan start in StartTimes)
+            {
+                endtime[i] = start + new TimeSpan(0, Durations[i], 0);
+                retst[i] = endtime[i].ToString();
+                i++;
+            }
+            string[] timeforconsult = new string[a];
+            //for(int time = 0; time<timeforconsult.Length; time++)
+            //{
+            // TimeSpan  t = BeginWorkingTime + new TimeSpan(0, ConsultationTime, 0);
+            //    if (t <= EndWorkingTime)
+            //    {
+                    
+            //        timeforconsult[time]=(BeginWorkingTime+"-"+t).ToString();
+            //        BeginWorkingTime = t;
+            //    }
+            //}
+            int o = 0;
+            for(int j=0;j<StartTimes.Length;j++)
+            {
+                while (BeginWorkingTime + new TimeSpan(0, ConsultationTime, 0) <= StartTimes[j])
+                {
+                    if(o<timeforconsult.Length)
+                    {
+                        timeforconsult[o] = BeginWorkingTime.ToString() + "-" + (BeginWorkingTime + new TimeSpan(0, ConsultationTime, 0)).ToString();
+                        BeginWorkingTime += new TimeSpan(0, ConsultationTime, 0);
+                        o++;
+                    }
+                    
+                }
+                BeginWorkingTime = endtime[j];
+            }
+            while(BeginWorkingTime + new TimeSpan(0, ConsultationTime, 0) <= EndWorkingTime)
+            {
+                timeforconsult[o] = BeginWorkingTime.ToString() + "-" + (BeginWorkingTime + new TimeSpan(0, ConsultationTime, 0)).ToString();
+                BeginWorkingTime += new TimeSpan(0, ConsultationTime, 0);
+                o++;
+            }
+
+           
+
+            return timeforconsult;
+            //return new string[2] { "iddqd", "idkfa" };
+        }
+    }
+}
